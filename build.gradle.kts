b3d92a354eb8c2851bec57d807c37eeffa7beaa5
akimov1712 <akimov1712@mail.ru>
2025-05-09 20:08:08 +0300

    chore: added ORM Exposed framework to project

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    kotlin("jvm") version "2.1.10"
+}
+
+group = "ru.akimov1712"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(libs.exposed.core)
+    implementation(libs.exposed.jdbc)
+    implementation(libs.mysql.connector)
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(18)
+}
